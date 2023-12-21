961a9be5c3aa9edca48793edeff882d2dd16e6ab
AdRavi89 <adwisravi@gmail.com>
2023-12-21 08:51:00 +0700

    editPassword - User

--- resources/views/user/editPassword.blade.php
+++ resources/views/user/editPassword.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            User &raquo; {{ $item->name }} &raquo; Edit Password
+            User &raquo; {{ $item->name }} &raquo; Edit
         </h2>
     </x-slot>
     <div class="py-12">
@@ -18,50 +18,45 @@ py-2">
 
 bg-red-100 px-4 py-3 text-red-700">
                         <p>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+                        <ul>
+
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                         </p>
                     </div>
                 </div>
                 @endif
-                <form class="w-full" action="{{ url('/user/update/'.$item->id) }}" method="post">
+                <form class="w-full" action="{{ url('/user/updatepassword/'.$item->id) }}" method="post">
                     @csrf
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700
 text-xs font-bold mb-2" for="grid-last-name">
-                                Name
+                                Password Baru
                             </label>
-
-                            <input value="{{ old('name') ?? $item->name }}" name="name" class="appearance-none block w-full bg-gray-200 text-gray-700 border
+                            <input value="{{ old('password') }}" name="password" class="appearance-none block w-full bg-gray-200 text-gray-700 border
 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white
-focus:border-gray-500" id="grid-last-name" type="text" placeholder="User Name">
+focus:border-gray-500" id="grid-last-name" type="password" placeholder="Password">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700
 text-xs font-bold mb-2" for="grid-last-name">
-                                Role
+                                Konfirmasi Password Baru
                             </label>
-
-                            <select name="role" class="appearance-none block w-full
-bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight
-focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name">
-                                
-                                <option value="CAMABA">CAMABA</option>
-                                <option value="UNIVERSITAS">UNIVERSITAS</option>
-                            </select>
+                            <input value="{{ old('Confirm_Password') }}" name="password_confirmation" class="appearance-none block w-full bg-gray-200 text-gray-700 border
+border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white
+focus:border-gray-500" id="grid-last-name" type="password" placeholder="Password">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
                         <div class="w-full px-3 text-right">
-                            <button type="submit" class="bg-green-500
+                            <button type="submit" class="bg-green-500   
 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                Update User
+                                Update Password
                             </button>
                         </div>
                     </div>
